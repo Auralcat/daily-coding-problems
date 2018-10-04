f98424015bc022239d6af75e2e9ae97a3088ea31
Auralcat <ashleyzt@hi2.in>
2018-10-04 19:25:29 -0300

    Set up tests with mocha and chai for Javascript

---
+++ JavaScript/tests/sample_test.js
@@ -0,0 +1,9 @@
+// Sample test file
+const chai = require('chai')
+const assert = chai.assert
+
+describe('Test the truth', function(){
+  it("specific test case", function() {
+    assert.equal(1 + 1, 2)
+  });
+});
